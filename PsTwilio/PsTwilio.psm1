98963caa2703c93cc7324a3cbc8b152887a9b0c3
Craig Buchanan <craig.buchanan@cogniza.com>
2021-07-27 20:24:38 -0500

    refactoring dot sourcing

--- PsTwilio/PsTwilio.psm1
+++ PsTwilio/PsTwilio.psm1
@@ -1,15 +1,13 @@
-@(
-    "$PSScriptRoot\Public\*.ps1"
-    # ",$PSScriptRoot\Private\*.ps1"
-) | Get-ChildItem |
+##
+# load (dot-source) *.PS1 files, excluding unit-test scripts (*.Tests.*), and disabled scripts (__*)
+#
+
+@("$PSScriptRoot\Public\*.ps1","$PSScriptRoot\Private\*.ps1") | 
+    Get-ChildItem -ErrorAction 'Continue' | 
+    Where-Object { $_.Name -like '*.ps1' -and $_.Name -notlike '__*' -and $_.Name -notlike '*.Tests*' } | 
     ForEach-Object {
 
-        Try {
-            Write-Debug "Dot sourcing $($_.FullName)"
-            . $_
-        }
-        Catch {
-            Write-Error -Message "Failed to import function $($_.FullName): $_"
-        }
+        # dot-source script
+        . $_
 
     }
